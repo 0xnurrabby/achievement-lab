1f7eb5c0c7b172b98ad3cf28441b655e690d5050
Nur Rabby Shuvo <106588083+0xnurrabby@users.noreply.github.com>
2026-05-10 09:14:00 +0600

    feat: add event topic constants

---
+++ utils/events.js
@@ -0,0 +1,5 @@
+// Event topic utilities
+export const ERC20_TRANSFER='0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+export const ERC20_APPROVAL='0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925'
+export const ERC721_TRANSFER='0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+export const isTransfer=log=>log.topics[0]===ERC20_TRANSFER
