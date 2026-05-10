1c21313ab8db27ed4526f12c57622cfe0ddb22da
Nur Rabby Shuvo <106588083+0xnurrabby@users.noreply.github.com>
2026-05-10 09:09:05 +0600

    feat: add Base chain constants

---
+++ data/constants.js
@@ -0,0 +1,5 @@
+// Base chain constants
+export const CHAIN_ID = 8453
+export const BLOCK_TIME = 2000
+export const ZERO = '0x'+'0'.repeat(40)
+export const MULTICALL3 = '0xcA11bde05977b3631167028862bE2a173976CA11'
